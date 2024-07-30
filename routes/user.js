a2c30052dac96e2dbfa5dcf052ed852d207a590d
Santos2175 <santoshgurung949@gmail.com>
2024-07-30 20:15:23 +0545

    Login verification added

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,22 @@ const User = require('../models/user')
 const router = Router()
 
 router.get('/signin',(req,res)=>{
-    return res.render('sigin')
+    return res.render('signin')
 })
 
 router.get('/signup',(req,res)=>{
     return res.render('signup')
 })
+
+
+router.post('/signin', async(req,res)=>{
+    const {email, password} = req.body
+    const user = User.matchPassword(email,password)
+
+    console.log('User', user)
+    return res.redirect('/')
+})
+
 
 router.post('/signup', async(req,res)=>{
     const {fullName, email, password} = req.body
@@ -21,6 +31,8 @@ router.post('/signup', async(req,res)=>{
 
     return res.redirect('/')
 })
+
+
 
 
 module.exports =router;
